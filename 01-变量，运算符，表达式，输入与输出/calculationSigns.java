aee09cfff1d9bab619e439b0bf3172e439d14cd3
YeJiu97 <yejiuwork@outlook.com>
2022-12-01 05:29:24 +1030

    expression

--- 01-变量，运算符，表达式，输入与输出/calculationSigns.java
+++ 01-变量，运算符，表达式，输入与输出/calculationSigns.java
@@ -4,16 +4,20 @@ public class calculationSigns {
 
         int a = 10, b = 20;
 
+        System.out.println("四则运算：");
         System.out.println(a + b);
         System.out.println(a - b);
         System.out.println(a * b);
-
         System.out.println(a / b);
 
+        System.out.println("取模运算:");
         System.out.println(a % b);  //取模运算符，向零整除后的余数，注意余数可能为负数
+
+        System.out.println("自增与自减：");
         System.out.println(++a);  // a++：先取值后加1；++a：先加1后取值
         System.out.println(--a);  // a--：先取值后减1；--a：先减1后取值
 
+        System.out.println("简写的几种方法：");
         System.out.println(a+=b); // a = a + b 可以简写为 a += b
         System.out.println(a-=b); // a = a - b 可以简写为 a -= b
         System.out.println(a*=b); // a = a * b 可以简写为 a *= b
---
+++ 01-变量，运算符，表达式，输入与输出/expression.java
@@ -0,0 +1,11 @@
+public class expression {
+
+    public static void main(String[] args){
+
+        int a = 10 * 20 - 30 / 40;  // 使用表达式
+        System.out.println(a);
+
+        System.out.println(12 + 21 / 23 * 12.3 % 11);
+    }
+
+}
